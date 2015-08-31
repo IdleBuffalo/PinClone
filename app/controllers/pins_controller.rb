91411451eba91bba636a0dfef96b86a21bb17ce6
Dejan Gajsek <dgajsek@gmail.com>
2015-08-31 06:32:47 -0600

    Addpins/users assocation

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,4 +1,6 @@
 class PinsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
 
@@ -6,28 +8,26 @@ class PinsController < ApplicationController
     @pins = Pin.all
   end
 
-
   def show
   end
 
-
   def new
-    @pin = Pin.new
-  end
+    @pin = current_user.pins.build
 
+  end
 
   def edit
   end
 
-
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
       render :new
     end
   end
+
 
 
   def update
@@ -37,7 +37,6 @@ class PinsController < ApplicationController
         render :edit
       end
   end
-
 
   def destroy
     @pin.destroy
@@ -51,6 +50,11 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
+    def correct_user
+    @pin = current_user.pins.find_by(params[:id])
+    redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    # if pin is nil it will redirect to pins path
+    end
 
     def pin_params
       params.require(:pin).permit(:description)
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,9 @@
 class Pin < ActiveRecord::Base
+	belongs_to :user 
 end
+
+#I've added user_id first by generating a new migration: 
+#rails generate migration add_user_id_to_pins user_id:integer:index
+# Then I've set up association so that Pins can belong to user. 
+# This means pins will have a user_id parameter as well
+# and also a "user has many pins" has_many :pins
