77865690b0232573d55ceafe1a6326061a52ee5f
Dejan Gajsek <dgajsek@gmail.com>
2015-08-31 06:58:03 -0600

    fix bugs in pins_controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -51,7 +51,7 @@ class PinsController < ApplicationController
     end
 
     def correct_user
-    @pin = current_user.pins.find_by(params[:id])
+    @pin = current_user.pins.find_by(id: params[:id])
     redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     # if pin is nil it will redirect to pins path
     end
